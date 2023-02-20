51586ecc7f8fdbb1703791a8e0e4844efc7f38aa
Rifqi Harits <rharits18@gmail.com>
2023-02-20 19:36:31 +0700

    tugasharits

---
+++ a.php/index.php
@@ -0,0 +1,21 @@
+<?php 
+$kata = "Topi";
+$kata2 = "Bundar";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1a</title>
+</head>
+<body>
+    <h2>
+        <i>
+            "<?php echo $kata ?> saya <?php echo $kata2 ?>1, <?php echo $kata2 ?> <?php echo $kata ?> saya."
+            
+        </i?>
+    </h2>
+</body>
+</html>
---
+++ b.php/index.php
@@ -0,0 +1,21 @@
+<?php
+$npm = "53";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1b</title>
+</head>
+<body>
+    <p>
+        Aku adalah angka <b><?php echo $npm ?></b><br>
+        Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $npm * 5 ?></b><br>
+        Jika aku dibagi 2, maka aku sekarang menjadi <b><?php echo $npm / 2 ?></b><br>
+        Jika aku ditambah 75, maka aku sekarang menjadi <b><?php echo $npm + 75 ?></b><br>
+        Jika aku kurang 20, maka aku sekarang menjadi <b><?php echo $npm - 20 ?><br>
+    </P>
+</body>
+</html>
